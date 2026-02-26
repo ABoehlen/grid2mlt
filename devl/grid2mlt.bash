45149ebf24b613eaeb6c7186767e933de42e32f6
ABoehlen <adrian.boehlen@swisstopo.ch>
2026-02-26 14:27:41 +0100

    new test release for handling large DTMs

--- devl/grid2mlt.bash
+++ devl/grid2mlt.bash
@@ -1,4 +1,20 @@
 #/bin/bash
+################################################################################################
+#
+# Filename:     grid2mlt.bash
+# Author:       Adrian Boehlen
+# Date:         26.02.2026
+# Version:      0.2
+#
+# Purpose:      konvertiert ein Hoehenmodell im Format ESRI ASCII GRID in ein Hoehenmodell
+#               im Format swisstopo MMBL
+#
+#               Voraussetzungen:
+#               - Das ESRI ASCII GRID muss im Schweizer Landeskoordinatensystem
+#                 CH1903 LV03 vorliegen
+#               - Benoetigt grid2mlt_v2.awk im gleichen Verzeichnis
+#
+################################################################################################
 
 # Parameter und Voraussetzungen pruefen
 if [ $# != 1 ]
@@ -19,7 +35,6 @@ then
   rm -rf tmp
 fi
 
-
 # in temporaer erstelltes Verzeichnis wechseln
 mkdir tmp
 cd tmp
@@ -28,7 +43,6 @@ cd tmp
 echo "ASCII Grid aufteilen..."
 awk 'NR%6==1{x="T"++i;}{print > x}'  ../$1
 files=$(ls . | wc -l)
-
 
 # Header aus dem ersten temporaeren File einlesen
 ncols=$(awk '$1 ~ /ncols|NCOLS/ {print $2}' T1)
@@ -43,11 +57,14 @@ rm -f T1
 # Liste der temporaeren Files in numerischer Reihenfolge erstellen
 ls -A1v > ../filelist.txt
 
+# Uebersetzung durchfuehren und Ergebnis im Hauptverzeichnis ablegen
+echo "ASCII Grid konvertieren..."
+awk -v ncols=$ncols -v nrows=$nrows -v xllcorner=$xllcorner -v yllcorner=$yllcorner -v cellsize=$cellsize -v NODATA_value=$NODATA_value -v files=$files -f ../grid2mlt_v2.awk $(<../filelist.txt) > ../out.mlt
 
-
-awk -v ncols=$ncols -v files=$files -f ../tmp.awk $(<../filelist.txt)
-
-
+echo "Konvertierung beendet. Temporaere Dateien werden geloescht"
+cd ..
+rm filelist.txt
+rm -rf tmp
 
 
 
